4018992e5b19223223181d3088af7531f35f478e
Alec Tessarowicz <atessarowicz@gmail.com>
2013-10-05 17:24:06 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HighcartsTest::Application.routes.draw do
-  get "pages/home"
+  root  'pages#home'
+  match '/home', to: 'pages#home', via 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
